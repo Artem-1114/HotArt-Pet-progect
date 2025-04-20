dadff47bcc83f8494536c37f30dd8358e4e6328d
Artem-1114 <artem.ishe1114@gmail.com>
2025-04-20 20:43:54 +0300

    Fix Netlify deployment

--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import './i18n/i18n';
-import { AuthProvider } from "./components/AuthContext";
-import { WishlistProvider } from "./components/WishlistContext";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // Змінив BrowserRouter на HashRouter
+import App from './App.jsx'
+import './index.css'
+import './i18n/i18n'
+import { AuthProvider } from './components/AuthContext'
+import { WishlistProvider } from './components/WishlistContext'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* Використовуємо HashRouter замість BrowserRouter */}
       <AuthProvider>
         <WishlistProvider>
           <App />
         </WishlistProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
